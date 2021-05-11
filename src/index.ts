ea366340dded287cdd5a286d3047911b205f87cd
Alexander Niebuhr <45965090+alexanderniebuhr@users.noreply.github.com>
2021-05-11 15:50:48 +0200

    wip: link action

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
 export { default as RouterView } from './RouterView.svelte'
+
+export function link (node: HTMLElement): {
+  update?: (parameters: any) => void
+  destroy?: () => void
+} {
+  node.addEventListener('click', function (event) {
+    event.preventDefault()
+    const href = node.getAttribute('href')
+    window.history.pushState({}, '', href)
+    useRouter.update(data => {
+      data.currentPath = (href != null) ? href : '/'
+      return data
+    })
+  })
+  return {
+    destroy () {
+    // the node has been removed from the DOM
+    }
+  }
+}
