6f35cc6e63fa630d754d202198e17b5710b86dd1
Alexander Niebuhr <tech@nbhr.io>
2021-08-24 15:03:12 +0200

    feat: :sparkles: add layout & rootPath support

--- src/node/node.ts
+++ src/node/node.ts
@@ -5,56 +5,64 @@ import type { PreprocessorGroup } from 'svelte/types/compiler/preprocess/types'
 interface Options {
   rootDir: string
   pageDir: string
-  mainComponent: string
+  rootPath: string
 }
 
 export function createRoutes(options: Options): PreprocessorGroup {
-  const { rootDir, pageDir, mainComponent } = options
+  const { rootDir, pageDir, rootPath } = options
   return {
-    script: ({ content, attributes }) => {
-      const routes: Map<string, unknown> = new Map()
-      const files = walkSync(resolve(join(rootDir, pageDir)))
-      let importScriptBlock = ''
-      for (const file of files) {
-        const parsedPath = relative(resolve(rootDir), file)
-        const directory = basename(dirname(parsedPath))
-        const base = basename(file)
-        let name = base.split('.')[0]
-        const isDynamic = name.includes('[')
-        name = name.replace('[', '').replace(']', '')
-        const componentName = isDynamic ? directory.toUpperCase() : name
-        const path = `"./${parsedPath.replaceAll('\\', '/')}"`
-        importScriptBlock += `\nimport ${componentName} from ${path};`
+    script: ({ content, attributes, filename }) => {
+      if (filename && filename.endsWith('App.svelte')) {
+        const routes: Map<string, unknown> = new Map()
+        const files = walkSync(resolve(join(rootDir, pageDir)))
+        let importScriptBlock = ''
+        for (const file of files) {
+          const parsedPath = relative(resolve(rootDir), file)
+          const directory = basename(dirname(parsedPath))
+          const base = basename(file)
+          let name = base.split('.')[0]
+          const isDynamic = name.includes('[')
+          name = name.replace('[', '').replace(']', '')
+          // TODO: use better Import, to allow multiple import of same name
+          const componentName = isDynamic ? directory.toUpperCase() : name
+          const path = `"./${parsedPath.replaceAll('\\', '/')}"`
+          importScriptBlock += `\nimport ${componentName} from ${path};`
 
         const routePath = `${
-          directory == pageDir ? '' : '/' + directory.toLowerCase()
-        }${isDynamic ? '/:' + name : '/' + name.toLowerCase()}`
+          const routePath = `${rootPath ? rootPath : ''}${
+            directory == pageDir ? '' : '/' + directory.toLowerCase()
+          }${isDynamic ? '/:' + name : '/' + name.toLowerCase()}`
+          const layoutName = attributes['router-layout-empty']
+            ? 'Empty'
+            : 'Default'
         console.log(routePath)
         
-        routes.set(componentName, {
-          path:
-            routePath == `/${mainComponent.split('.')[0].toLowerCase()}`
-              ? '/'
-              : routePath,
-          // pattern: new RegExp(pattern).toString(),
-          component: componentName,
-        })
-      }
+          routes.set(componentName, {
+            path: routePath,
+            // pattern: new RegExp(pattern).toString(),
+            component: componentName,
+            layout: layoutName,
+          })
+        }
 
-      const routesDefinition = `
-      $useRouter.routes = ${JSON.stringify([...routes.values()]).replace(
-        /(?<=component":)"(\w+)"/g,
-        '$1'
-      )};
+        const routesDefinition = `
+      $useRouter.routes = ${JSON.stringify([...routes.values()])
+        .replace(/(?<=component":)"(\w+)"/g, '$1')
+        .replace(/(?<=layout":)"(\w+)"/g, '$1')};
       `
 
-      let processedContent = content
-      if (attributes['svelteuse:imports'] === true) {
-        processedContent =
-          importScriptBlock + '\n' + routesDefinition + '\n\n' + content
-      }
-      return {
-        code: processedContent,
+        let processedContent = content
+        if (attributes['svelteuse:imports'] === true) {
+          processedContent =
+            importScriptBlock + '\n' + routesDefinition + '\n\n' + content
+        }
+        return {
+          code: processedContent,
+        }
+      } else {
+        return {
+          code: content,
+        }
       }
     },
   }
